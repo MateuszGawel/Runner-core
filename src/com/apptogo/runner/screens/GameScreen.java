84bb961b186baa719addc7133c47c556a8011023
mantkowicz <antek15@gmail.com>
2014-05-23 01:01:10 +0200

    polepszony render map (teraz warstwa musi mieć 'physicsEnabled' = true , żeby była obłożona ciałami - niestety wygląda jednak na to, że trzeba będzie obrysowywać wszystko ale jeszcze nad tym popracuje)

--- src/com/apptogo/runner/screens/GameScreen.java
+++ src/com/apptogo/runner/screens/GameScreen.java
@@ -1,8 +1,8 @@
 package com.apptogo.runner.screens;
 
 import com.apptogo.runner.handlers.Logger;
-import com.apptogo.runner.handlers.PhysicsGenerator;
 import com.apptogo.runner.handlers.ScreensManager.ScreenType;
+import com.apptogo.runner.handlers.TiledMapLoader;
 import com.apptogo.runner.main.Runner;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -26,14 +26,14 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GameScreen extends BaseScreen{
 	
-	private OrthogonalTiledMapRenderer tiledMapRendered;
-	TiledMap tileMap;
+	private final boolean DRAW_DEBUG = true;
+		
 	private Stage stage;
 	private Viewport viewport;
+	private Box2DDebugRenderer debugRenderer;
 	
 	private World world;
-	private Box2DDebugRenderer debugRenderer;
-	private PhysicsGenerator physicsGenerator;
+	private OrthogonalTiledMapRenderer tiledMapRenderer;
     
 	public GameScreen(Runner runner){
 		super(runner);	
@@ -43,45 +43,15 @@ public class GameScreen extends BaseScreen{
 	public void show() {
 		
 		stage = new Stage();
-		
-		TmxMapLoader loader = new TmxMapLoader();
-		tileMap = loader.load( "gfx/game/levels/map.tmx" );
-		
-		tiledMapRendered = new OrthogonalTiledMapRenderer( tileMap );
-		
 		viewport = new StretchViewport( 800, 480, camera);
 		
-		world = new World(new Vector2(0f, -9.2f), true);
+		if(DRAW_DEBUG) debugRenderer = new Box2DDebugRenderer();
 		
-		//body definition
-		BodyDef bdef = new BodyDef();
-		bdef.type = BodyType.DynamicBody;
-		bdef.position.set( new Vector2(100f, 400f) );
-		
-		//shape definition
-		PolygonShape shape = new PolygonShape();
-		shape.setAsBox(50, 50);
-		
-		//fixture definition
-		FixtureDef fdef = new FixtureDef();
-		fdef.shape = shape;
-		fdef.density = 1f;
-		fdef.friction = 0.5f;
-		fdef.restitution = 1f;
-		
-		//creating body
-		Body body = world.createBody(bdef);
-		body.createFixture(fdef);
-		
-		
-		
-		debugRenderer = new Box2DDebugRenderer();
-
-		
-		
-		physicsGenerator = new PhysicsGenerator(world);
-		physicsGenerator.createPhysics(tileMap);		
-		
+		if( TiledMapLoader.getInstance().load( "gfx/game/levels/map.tmx" ) )
+		{		
+			tiledMapRenderer = TiledMapLoader.getInstance().getMapRenderer();
+			world = TiledMapLoader.getInstance().getWorld();
+		}
 	}
 	
 	@Override
@@ -89,14 +59,14 @@ public class GameScreen extends BaseScreen{
 
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		world.step(1/60f, 1, 1);
-	    //spriteBatch.begin();
 			    
-		tiledMapRendered.setView( (OrthographicCamera) viewport.getCamera() );
-	    tiledMapRendered.render();
-
+		tiledMapRenderer.setView( (OrthographicCamera) viewport.getCamera() );
+	    tiledMapRenderer.render();
+	    
 	    spriteBatch.begin();
-	    debugRenderer.render(world, viewport.getCamera().combined);
+	    	if(DRAW_DEBUG) debugRenderer.render(world, viewport.getCamera().combined);
 	    spriteBatch.end();
 	    
 	}
