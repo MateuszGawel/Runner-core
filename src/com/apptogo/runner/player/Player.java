b87b3c0c017bbecae43d279e504fde0f7d3036b1
mantkowicz <antek15@gmail.com>
2014-07-30 16:41:02 +0200

    zapomniałem o mergu

---
+++ src/com/apptogo/runner/player/Player.java
@@ -0,0 +1,19 @@
+package com.apptogo.runner.player;
+
+import com.apptogo.runner.controller.Controller;
+
+public class Player 
+{
+
+	private String name;
+	private String password;
+	
+	private Statistics statistics;
+	
+	public Controller controller;
+	
+	public Player()
+	{
+		
+	}	
+}
